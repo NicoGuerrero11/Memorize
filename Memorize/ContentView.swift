203a48a11e45c64e5b28f612045eb657b9984c23
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-08-13 21:48:30 -0600

    initial commit CardView CardConfig

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 0.96, green: 0.94, blue: 0.91)
+            VStack {
+                CardView()
+                CardView()
+                CardView()
+            }
+            .padding()
+            
         }
-        .padding()
+        
+        
+    }
+}
+
+struct CardView: View{
+    var body: some View{
+        HStack{
+            CardConfig(isClick: true)
+            CardConfig()
+            CardConfig()
+        }
+        .foregroundColor(Color(red: 0.0, green: 0.55, blue: 0.55))
+    }
+}
+
+struct CardConfig: View{
+    var isClick: Bool = false
+    var body: some View{
+        
+        ZStack{
+            let base = Rectangle()
+            
+            if isClick{
+                base.fill(Color(red: 0.87, green: 0.91, blue: 0.90))
+                base.strokeBorder(lineWidth: 4)
+            }
+            else{
+                base.fill()
+
+            }
+        }
+        
+        
     }
 }
 
